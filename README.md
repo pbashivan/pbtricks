2f71320ed6772de4a0cf9d7bc62fd925bb5d443c
pbashivan <poya.bashivan@gmail.com>
2018-03-05 18:00:42 -0500

    This is how it started...

---
+++ README.md
@@ -0,0 +1,11 @@
+# PBTricks
+Everyday use functions.
+![alt text](diagram.png "Converting EEG recordings to movie snippets")
+
+# How to install
+
+`pip install [path_to_EEGLearn]`
+
+# How to use
+`import pbtricks`
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PBTricks',
+    version='0.1',
+    packages=['pbtricks'],
+    install_requires=['numpy', 'scipy'],
+    url='https://github.com/pbashivan/pbtricks',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Pouya Bashivan',
+    description="Pouya's bag of tricks >:)"
+)
