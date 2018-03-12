694c57ea2732369f9e5fe59b7c1f9ae85a2d3dcb
pbashivan <poya.bashivan@gmail.com>
2018-03-12 17:58:35 -0400

    Added image packing func

--- pbtricks/data.py
+++ pbtricks/data.py
@@ -1,7 +1,38 @@
+from __future__ import print_function
 import pickle
+from scipy.misc import imread
+import os
+import h5py
+import numpy as np
 
 
 def unpickle(file):
   with open(file, 'rb') as fo:
     dict = pickle.load(fo, encoding='bytes')
   return dict
+
+
+def pack_images_h5(images_path, output_path=None, file_type='png', image_names=None):
+  """
+  Packs a set of images in the given directory
+  :param images_path: path to directory containing the images
+  :param output_path: path to save the output file
+  :param file_type: file extension name: "png"
+  :param image_names: list containing name of all images (default: None -->‌ all the images found under the path)
+  :return: None
+  """
+  images = []
+  if image_names is None:
+    files_list = sorted(os.listdir(images_path))
+    for filename in files_list:
+      if filename.endswith("." + file_type):
+        images.append(imread(filename))
+  else:
+    for idx in image_names:
+      filename = os.path.join(images_path, idx + '.' + file_type)
+      images.append(imread(filename))
+  if output_path is None:
+    output_path = images_path
+  with h5py.File(os.path.join(output_path, 'images.h5'), 'w') as h5file:
+    h5file.create_dataset('images', data=np.array(images))
+  print('Finished saving.')
