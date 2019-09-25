f732c2b69338f6d905f8158cb5eadc24fb36a17d
pbashivan <poya.bashivan@gmail.com>
2019-09-25 17:48:32 -0400

    Added function to sort values in Pandas DF column

--- pbtricks/misc.py
+++ pbtricks/misc.py
@@ -20,3 +20,20 @@ def predict_mapping_outputs(features, weights):
   preds = np.matmul(model_pcs, weights['pls_w']) + weights['pls_b']
   return preds
 
+
+def sort_df_rows(df, column_name, sorted_list):
+  """
+  Sort values in a column give the sorted list values
+  :param df: dataframe object
+  :param column_name: string containing the column name
+  :param sorted_list: a list containing the ordered list of values
+  :return: dataframe object
+  """
+  tmp_df = df.copy(deep=True)
+  sorterIndex = dict(zip(sorted_list, range(len(sorted_list))))
+  tmp_df['sorter_list'] = tmp_df[column_name].map(sorterIndex)
+
+  tmp_df.sort_values(['sorter_list'], inplace=True)
+  tmp_df.drop('sorter_list', 1, inplace=True)
+  _ = tmp_df.reset_index(drop=True)
+  return tmp_df
